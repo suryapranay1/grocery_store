9d0cd0b5d29ff7093e79ebbc02b5726177ba9c5c
Surya Pranay <suryapranay2020@gmail.com>
2025-04-25 23:42:00 +0530

    setup banner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 const App = () => {
-  return <Navbar></Navbar>;
+  const isSellerPath = useLocation().pathname.includes("seller");
+  return (
+    <>
+      {isSellerPath ? null : <Navbar></Navbar>}
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
